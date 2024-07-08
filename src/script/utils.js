41eca234b9512b57ac7b2a5ed7fdad6048b9e22a
RobertBenyamin <robertbenyamino@gmail.com>
2024-07-09 01:24:45 +0700

    featr: Update Utils class with new method formatDate

--- src/script/utils.js
+++ src/script/utils.js
@@ -1,22 +1,27 @@
 class Utils {
-    static emptyElement(element) {
-      element.innerHTML = '';
-    }
-  
-    static showElement(element) {
-      element.style.display = 'block';
-      element.hidden = false;
-    }
-  
-    static hideElement(element) {
-      element.style.display = 'none';
-      element.hidden = true;
-    }
-  
-    static isValidInteger(newValue) {
-      return Number.isNaN(newValue) || Number.isFinite(newValue);
-    }
+  static emptyElement(element) {
+    element.innerHTML = "";
   }
-  
-  export default Utils;
-  
+
+  static showElement(element) {
+    element.style.display = "block";
+    element.hidden = false;
+  }
+
+  static hideElement(element) {
+    element.style.display = "none";
+    element.hidden = true;
+  }
+
+  static isValidInteger(newValue) {
+    return Number.isNaN(newValue) || Number.isFinite(newValue);
+  }
+
+  static formatDate(dateString) {
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    const date = new Date(dateString);
+    return date.toLocaleDateString("id-ID", options);
+  }
+}
+
+export default Utils;
